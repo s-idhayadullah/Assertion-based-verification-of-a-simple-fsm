3d24faee111fe910093b27dbebfe986e2f66952b
s-idhayadullah <idhayasheik00@gmail.com>
2026-03-31 11:25:18 +0530

    Update and rename fsm_tb.sv to fsm_tb(Directed_input_Stimuli).sv

--- with no cover/fsm_tb(Directed_input_Stimuli).sv
+++ with no cover/fsm_tb(Directed_input_Stimuli).sv
@@ -1,5 +1,5 @@
-module tb();
-
+module tb();        // Directed test, the only aim to acheive 100% coverage     
+                                     
     reg clk;
     reg rst_n;
     reg req;
@@ -36,57 +36,57 @@ end
 
     #10 rst_n = 1;
 
-    // -------------------------
+
     // 1. NORMAL FLOW
-    // -------------------------
+    
     #10 req = 1; #10 req = 0;
     #10 grant = 1; #10 grant = 0;
     #10 addr_valid = 1; #10 addr_valid = 0;
     #10 data_ready = 1; #10 data_ready = 0;
 
-    // -------------------------
+        
     // 2. ARBIT CANCEL
-    // -------------------------
+    
     #20 req = 1; #10 req = 0;
     #10 cancel = 1; #10 cancel = 0;
 
-    // -------------------------
+    
     // 3. ARBIT WAIT (no grant)
-    // -------------------------
+    
     #20 req = 1; #10 req = 0;
     #30; // stay in ARBIT
 
-    // -------------------------
+    
     // 4. ADDR HOLD
-    // -------------------------
+    
     #10 grant = 1; #10 grant = 0;
     #30; // addr_valid = 0
 
-    // -------------------------
+    
     // 5. DATA → ERROR
-    // -------------------------
+    
     #10 addr_valid = 1; #10 addr_valid = 0;
     #10 error = 1; #10 error = 0;
 
-    // -------------------------
+    
     // 6. DATA → TIMEOUT
-    // -------------------------
+    
     #20 req = 1; #10 req = 0;
     #10 grant = 1; #10 grant = 0;
     #10 addr_valid = 1; #10 addr_valid = 0;
     #10 timeout = 1; #10 timeout = 0;
 
-    // -------------------------
+    
     // 7. DATA WAIT (no signals)
-    // -------------------------
+    
     #20 req = 1; #10 req = 0;
     #10 grant = 1; #10 grant = 0;
     #10 addr_valid = 1; #10 addr_valid = 0;
     #30; // no data_ready/error/timeout
 
-    // -------------------------
+    
     // 8. DATA → COMPLETE again
-    // -------------------------
+    
     #10 data_ready = 1; #10 data_ready = 0;
 
     #50 $finish;
